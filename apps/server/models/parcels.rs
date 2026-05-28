c4a936e95778a77592e48a4ff69abd56c1f41fbd
OscarPastry <pattanayakrajarshi@gmail.com>
2026-05-28 23:37:08 +0530

    added models

---
+++ apps/server/models/parcels.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum ParcelStatus {
+    Registered,
+    Pending,
+    Disputed,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct ParcelResponse {
+    pub id: String,
+    pub owner: String,
+    pub polygon: Vec<[f64; 2]>,
+    pub geohashes: Vec<string>,
+    pub area: f64,
+    pub location: String,
+    pub status: ParcelStatus,
+    pub token_id: Option<String>,
+    pub created_at: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateParcelRequest {
+    pub polygon: Vec<[f64; 2]>,
+    pub location: String,
+    pub owner_addr: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TransferParcelReq {
+    pub new_owner_addr: String,
+}
---
+++ apps/server/models/registry.rs
@@ -0,0 +1,25 @@
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct VerifyParcelResponse {
+    pub valid: bool,
+    pub message: String,
+    pub parcel_id: string,
+}
+
+#[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct TransferRecord {
+    pub from: String,
+    pub to: String,
+    pub timestamp: String,
+}
+
+#[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RegistryHistoryResponse {
+    pub parcels_id: String,
+    pub transfer_records: Vec<TransferRecord>,
+    pub message: String,
+}
