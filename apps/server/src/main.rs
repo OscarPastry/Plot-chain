7d60a27b3f9478b0657472ceb74ba1bcc437a0c8
OscarPastry <pattanayakrajarshi@gmail.com>
2026-05-28 21:04:16 +0530

    documentation

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,10 +1,26 @@
+//Axum server main point
+//
 use axum::{Json, Router, routing::get};
+//Router → defines routes/endpoints
+//routing::get → specifies a GET request handler
+//Json → converts Rust structs into JSON responses
+//
 use serde::Serialize;
+// Serialize → allows us to convert Rust structs into JSON format for API responses
+//
 use std::net::SocketAddr;
+// SocketAddr → represents an IP address and port combination for the server to listen on
+//
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
+// CorsLayer → handles Cross-Origin Resource Sharing (CORS) to allow requests from different origins
+// TraceLayer → provides logging and tracing for incoming HTTP requests
+//
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+// tracing_subscriber → sets up logging and tracing for the application, allowing us to monitor and debug the server's behavior
+//
 
 #[derive(Serialize)]
+//automatically generates code to convert the struct into JSON.
 struct HealthResponse {
     status: &'static str,
     service: &'static str,
@@ -20,15 +36,17 @@ async fn health() -> Json<HealthResponse> {
 }
 
 #[tokio::main]
-
+//This macro:
+//1.creates the async runtime
+//2.allows main to be async
 async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new("info, tower_http=info"))
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer()) // Pretty-prints logs to terminal
         .init();
-    let app = Router::new()
+    let app = Router::new() // Creates a new Axum router
         .route("/api/health", get(health))
-        .layer(CorsLayer::permissive())
+        .layer(CorsLayer::permissive()) // Allows request from any origin
         .layer(TraceLayer::new_for_http());
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::info!("Listening on {}", addr);
