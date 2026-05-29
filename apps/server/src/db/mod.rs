b38321ceda103c8d5a9db2f63e00420f383e7cde
OscarPastry <pattanayakrajarshi@gmail.com>
2026-05-29 21:55:42 +0530

    database integration

---
+++ apps/server/src/db/mod.rs
@@ -0,0 +1,14 @@
+pub mod parcels;
+
+use anyhow::{Context, Result};
+use sqlx::{PgPool, postgres::PgPoolOptions};
+
+pub async fn connect_db(database_url: &str) -> Result<PgPool> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await
+        .context("failed to connect to postgres")?;
+
+    Ok(pool)
+}
